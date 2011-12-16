f1509dff40cec40e1fb896fc617441a9c0f08a42
aki <aki@xps>
2011-12-16 19:06:01 +0900

    add built

--- src/com/akjava/gwt/html5/client/HTML5InputRange.java
+++ src/com/akjava/gwt/html5/client/HTML5InputRange.java
@@ -15,14 +15,28 @@
  */
 package com.akjava.gwt.html5.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.HasChangeHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.FocusWidget;
 
-public class HTML5InputRange extends FocusWidget{
+public class HTML5InputRange extends FocusWidget implements HasChangeHandlers{
 
 	public HTML5InputRange(int min,int max,int current){
 		super(RangeElement.createRangeElement(Document.get(),min,max,current));
+		this.addChangeHandler(new ChangeHandler() {
+			
+			@Override
+			public void onChange(ChangeEvent event) {
+				fireEvent();
+			}
+		});
 	}
 	 protected RangeElement getRangeElement() {
 		    return getElement().cast();
@@ -30,8 +44,36 @@ public class HTML5InputRange extends FocusWidget{
 	 public int getValue(){
 		 return Integer.parseInt(getRangeElement().getValue());
 	 }
+	 public void setMax(int max){
+		 getRangeElement().setMax(max);
+	 }
 	 public void setValue(int value){
 		 getRangeElement().setValue(value);
+		 
+		 fireEvent();
+		 /*
+		  * trying  fire event
+		 NativeEvent nativeEvent=(NativeEvent) NativeEvent.createObject();
+		 ChangeEvent.fireNativeEvent(nativeEvent, this);
+		 */
+	 }
+	 
+	 public static interface HTML5InputRangeListener {
+		 public void changed(int newValue);
+	 }
+	 
+	 private List<HTML5InputRangeListener> listeners=new ArrayList<HTML5InputRangeListener>();
+	 public synchronized void addListener(HTML5InputRangeListener listener){
+		 listeners.add(listener);
+	 }
+	 public synchronized void removeListener(HTML5InputRangeListener listener){
+		 listeners.remove(listener);
+	 }
+	 
+	 public synchronized void fireEvent(){
+		 for(HTML5InputRangeListener listener:listeners){
+			 listener.changed(getValue());
+		 }
 	 }
 	 
 	 public static class RangeElement extends Element{
@@ -63,5 +105,15 @@ public class HTML5InputRange extends FocusWidget{
 		    e.value=value;
 		    return e;
 		  }-*/;
+	}
+
+	 /*
+	  * better to use addListener
+	  * (non-Javadoc)
+	  * @see com.google.gwt.event.dom.client.HasChangeHandlers#addChangeHandler(com.google.gwt.event.dom.client.ChangeHandler)
+	  */
+	@Override
+	public HandlerRegistration addChangeHandler(ChangeHandler handler) {
+	    return addDomHandler(handler, ChangeEvent.getType());
 	}
 }
---
+++ src/com/akjava/gwt/html5/client/extra/HTML5Builder.java
@@ -0,0 +1,42 @@
+package com.akjava.gwt.html5.client.extra;
+
+import com.akjava.gwt.html5.client.HTML5InputRange;
+import com.akjava.gwt.html5.client.HTML5InputRange.HTML5InputRangeListener;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.ui.Label;
+
+public class HTML5Builder {
+	public static Label createRangeLabel(final String text,final HTML5InputRange range){
+		final Label label=new Label();
+		label.setText(text+""+range.getValue());
+		label.setStylePrimaryName("title");
+		range.addListener(new HTML5InputRangeListener() {
+			@Override
+			public void changed(int newValue) {
+				label.setText(text+""+newValue);
+			}
+		});
+		
+		/*
+		range.addChangeHandler(new ChangeHandler() {
+			
+			@Override
+			public void onChange(ChangeEvent event) {
+				label.setText(text+":"+range.getValue());
+				
+			}
+		});*/
+		/*
+		range.addMouseUpHandler(new MouseUpHandler() {
+			
+			@Override
+			public void onMouseUp(MouseUpEvent event) {
+				label.setText(text+":"+range.getValue());
+			}
+		});
+		*/
+		
+		return label;
+	}
+}
