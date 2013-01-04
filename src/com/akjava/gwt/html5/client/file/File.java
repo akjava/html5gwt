40a4829a2fd766d0f85597351de0cd46eeeb7581
aki <aki@xps>
2013-01-04 20:34:44 +0900

    add slice

--- src/com/akjava/gwt/html5/client/file/File.java
+++ src/com/akjava/gwt/html5/client/file/File.java
@@ -15,7 +15,6 @@
  */
 package com.akjava.gwt.html5.client.file;
 
-import com.google.gwt.core.client.JavaScriptObject;
 
 public class File extends Blob{
 
@@ -33,5 +32,9 @@ public class File extends Blob{
 	
 	public  final native String getFileType()/*-{
     return this.type;
+  }-*/;
+	
+	public  final native Blob slice(int start,int length)/*-{
+    return this.slice(start,length);
   }-*/;
 }
