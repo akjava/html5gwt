971e623a7d7abbbff2ac69ceeacfab1066234b06
aki <aki@xps>
2013-06-01 15:56:49 +0900

    disable selection

--- src/com/akjava/gwt/html5/client/CanvasInputRange.java
+++ src/com/akjava/gwt/html5/client/CanvasInputRange.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
@@ -167,7 +168,13 @@ private boolean enabled=true;
 		});
 		lastValue=current+1;
 		update(current);
+		disableSelectionStart(canvas.getElement());
 	}
+	
+	public native final static void disableSelectionStart(Element element)/*-{
+	element.onselectstart = function () { return false; }
+
+	}-*/;
 	
 	int lastValue;
 	private void update(int value){
