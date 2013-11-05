98f952c7486863ba5435cc089786d91b8a285143
aki <aki@xps>
2013-11-05 20:56:58 +0900

    add filenameandtext

---
+++ src/com/akjava/gwt/html5/client/file/ui/FileNameAndText.java
@@ -0,0 +1,31 @@
+package com.akjava.gwt.html5.client.file.ui;
+
+import com.akjava.gwt.html5.client.download.HTML5Download;
+import com.google.gwt.user.client.ui.Anchor;
+
+public class FileNameAndText {
+private String name;
+private String text;
+public FileNameAndText(){}
+public FileNameAndText(String name,String text){
+	this.name=name;
+	this.text=text;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getText() {
+	return text;
+}
+public void setText(String text) {
+	this.text = text;
+}
+
+public Anchor createDownloadLink(String linkText){
+	return new HTML5Download().generateTextDownloadLink(text, name, linkText);
+}
+
+}
---
+++ src/com/akjava/gwt/html5/client/file/ui/FileNameAndTextCell.java
@@ -0,0 +1,17 @@
+package com.akjava.gwt.html5.client.file.ui;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+
+public class FileNameAndTextCell extends AbstractCell<FileNameAndText>{
+
+	@Override
+	public void render(com.google.gwt.cell.client.Cell.Context context,
+			FileNameAndText value, SafeHtmlBuilder sb) {
+		 if(value == null){
+		      return;
+		    }
+		 sb.appendHtmlConstant(value.getName());
+	}
+
+}
